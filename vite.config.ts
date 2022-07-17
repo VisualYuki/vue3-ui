7c28ceda0913973294ee4315753b5e238bbe901a
den <moshkin.denis.007@yandex.ru>
2022-07-17 13:24:56 +0300

    remove template useless code, setting up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,38 @@
-import { fileURLToPath, URL } from 'url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {fileURLToPath, URL} from "url";
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src/components", import.meta.url)),
+		},
+	},
+	server: {
+		host: true,
+		port: 9090,
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src", "entry.ts"),
+			name: "vue3ui",
+			formats: ["iife", "es", "umd", "cjs"],
+			fileName: (format) => `vue3-ui.${format}.js`,
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ["vue"],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					vue: "Vue",
+				},
+			},
+		},
+	},
+});
